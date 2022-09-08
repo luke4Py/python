76471db0139395527d847af9be1b0ddf223a2614
Luke Samuel <lukesam9@gmail.com>
2022-09-08 21:58:18 +0530

    Python variables

---
+++ python_snippets/variables_Docstrings.py
@@ -0,0 +1,166 @@
+# B
+# ->syntax
+
+variable_name = 'value'
+
+
+paper1 = 'maths'
+paper2 = 'Science'
+paper3 = 'Social'
+
+"Vamshi details"
+eduction = 'Btech'
+percent = 80
+spec = 'Civil'
+
+
+print(eduction)
+print(percent)
+print(spec)
+
+
+
+print(paper1)
+print(paper2)
+print(paper3)
+
+paper4 = 'english'  # string
+paper5 = 123  # integer
+paper6 = 123.5  # float
+paper7 = 3j+2  # complex
+
+print(paper4)
+print(paper5)
+print(paper6)
+print(paper7)
+
+# A) Variables
+
+Deep = 1 
+grishma = 2
+niyas = 3
+murali = 4
+keerthika = 5
+
+
+
+xyz = 25652222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222
+print(xyz)
+
+wiki = """Interstate 82 (I-82) is an Interstate Highway 
+        in the Pacific Northwest region of the United States 
+        that passes through parts of Washington and Oregon. 
+        It runs southeasterly for 144 miles (232 km) from 
+        I-90 in Ellensburg, Washington, to I-84 in Hermiston, 
+        Oregon. The highway passes through Yakima, the 
+        Yakima Valley agricultural region, and the 
+        Tri-Cities area, also serving as part of the 
+        link between Seattle and Boise, Idaho. 
+        It crosses the Columbia River on the Umatilla Bridge (pictured), 
+        which opened in 1955 and was later twinned. 
+        The corridor was originally used by several state 
+        and national highways until I-82 was created in 
+        late 1957. The first section to be constructed was 
+        a bypass of Yakima, opened in 1963, which was 
+        extended north to Ellensburg on November 12, 1971.
+        Valley section, connecting Union Gap to Prosser, 
+        was constructed between 1977 and 1982. 
+        The Tri-Cities bypass opened in 1986, 
+        delayed by a lengthy routing dispute, and the 
+        final section was completed in 1988. 
+        (Full article...) """
+
+print(wiki)
+
+
+# COMMON VARIABLE NAMES
+"""
+a,b,c
+i,j,k
+x,y,z
+"""
+"""It is always better to create variable names according to context. As you can observe below"""
+
+# 1)Valid Variable Names
+
+# Rules
+'''
+#no special characters in variables other than _
+@varible = 55
+%varibale = 55
+var%ible = 25
+&variable = 55
+*variable = 55
+
+Deepanshu@429
+
+#no numbers in the begining of a variable name
+1variable = 55
+25variable = 25
+
+'''
+
+# possible variable names
+variable = 55
+_variable = 55
+V_jkrng = 5
+V1234434446346555 = 45
+V_123lku = 123
+
+
+
+# 2) Multiple Assignments
+maths = 15 #storing value in maths variable 
+science = 20
+social = 25
+
+print(maths)
+print(science)
+print(social)
+
+maths, science, social = 25, 20, 15
+print(maths)
+print(science)
+print(social)
+
+
+student1, student2, student3 = 'nanda', 'nadini', 'jeevi'
+
+print(student1)
+print(student2)
+print(student3)
+
+
+# 3) Swapping variables
+"M1 Maths Marks" '25'
+
+keerti = 15
+Deep = 20 
+"""swaping values 15,20 between keerti and Deep""" 
+
+print(keerti)
+print(Deep)
+
+keerti, Deep = Deep, keerti
+
+print(keerti)
+print(Deep)
+
+
+student1, student2, student3 = 'venu', 'priya', 'vidya'
+
+print(student1, student2, student3)
+
+student1, student2, student3 = student2, student1, student3
+
+
+#Literals in python 
+"String literals  -> str values that we store in variables." 
+'Numeric Literals -> int and float values that we store in variables.'
+'Boolean Literals -> Boolean(True/False) values that we store in variables.'
+
+#Identifiers in python
+"""
+An identifier is a name given to entities like class, functions, variables, etc. 
+It helps to differentiate one entity from another.
+"""
